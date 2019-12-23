27640b25498ed8dd521a0793ff782d62931d1c59
zhengr <ruizheng1119@gmail.com>
2019-12-23 16:10:56 +0800

    配置+controller

--- src/main/java/com/zhengrui/luckymoney/HelloController.java
+++ src/main/java/com/zhengrui/luckymoney/HelloController.java
@@ -2,17 +2,21 @@ package com.zhengrui.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
     @Autowired
     private LimitConfig limitConfig;
-    @GetMapping("/hello")
-    public String say(){
-        return "说明："+limitConfig.getDescription();
+//    @GetMapping("/say")
+    @PostMapping("/say")
+//    @RequestMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id){
+//        return "说明："+limitConfig.getDescription();
+        return "id:"+id;
     }
 }
